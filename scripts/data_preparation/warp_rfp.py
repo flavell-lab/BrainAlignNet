d1fba5b12584e05ec576ada0ae5693a10276cd31
Alicia Lu <lualicia88@gmail.com>
2024-07-17 20:12:48 +0000

    prep roi labels

---
+++ scripts/data_preparation/warp_rfp.py
@@ -0,0 +1,199 @@
+from data_utils import filter_and_crop, get_image_T
+from euler_gpu.preprocess import initialize
+from euler_gpu.transform import transform_image_3d
+from euler_register import EulerRegistrationProcessor
+from typing import Dict
+import glob
+import h5py
+import json
+import numpy as np
+import torch
+
+
+class RFPImageWarper(EulerRegistrationProcessor):
+
+    def __init__(self, *args, **kwargs):
+
+        super().__init__(euler_search=False, *args, **kwargs)
+        self.batch_size = None
+        self.downsample_factor = None
+        self.CM_dict = self._load_json(
+                "resources/center_of_mass.json")
+        self.euler_parameters_dict = self._load_json(
+                "resources/euler_parameters.json")
+        self.memory_dict_xy, self._memory_dict_xy = {}, {}
+
+    def _load_json(self, file_path: str):
+        """Load JSON resources"""
+        with open(file_path, "r") as f:
+            return json.load(f)
+
+    def process_datasets(self, augment: bool = False):
+
+        with open("resources/registration_problems.json", 'r') as f:
+            registration_problem_dict = json.load(f)
+
+        for dataset_type, problem_dict in registration_problem_dict.items():
+            self.process_dataset_type(dataset_type, problem_dict)
+
+    def process_problem(
+        self,
+        problem_id: str,
+        dataset_path: str,
+        hdf5_m_file: h5py.File,
+        hdf5_f_file: h5py.File,
+    ) -> Dict[str, np.ndarray]:
+
+        t_moving, t_fixed = problem_id.split('/')[1].split('to')
+        t_moving_4 = t_moving.zfill(4)
+        t_fixed_4 = t_fixed.zfill(4)
+        # RFP images: /NRRD_filtered/*_ch2.nrrd
+        # GFP images: /NRRD_cropped/*_ch1.nrrd
+        fixed_image_path = glob.glob(
+            f'{dataset_path}/NRRD_cropped/*_t{t_fixed_4}_ch1.nrrd')[0]
+        moving_image_path = glob.glob(
+            f'{dataset_path}/NRRD_cropped/*_t{t_moving_4}_ch1.nrrd')[0]
+        fixed_image_T = get_image_T(fixed_image_path)
+        fixed_image_median = np.median(fixed_image_T)
+        moving_image_T = get_image_T(moving_image_path)
+        moving_image_median = np.median(moving_image_T)
+
+        resized_fixed_image_xyz = filter_and_crop(
+                fixed_image_T,
+                fixed_image_median,
+                self.target_image_shape
+        )
+        resized_moving_image_xyz = filter_and_crop(
+                moving_image_T,
+                moving_image_median,
+                self.target_image_shape
+        )
+        euler_transformed_moving_image = self._euler_transform_image(
+                resized_moving_image_xyz,
+                problem_id
+        )
+        return {
+            "fixed_image": resized_fixed_image_xyz,
+            "moving_image": euler_transformed_moving_image
+        }
+
+    def _euler_transform_image(
+        self,
+        moving_image: np.ndarray,
+        problem_id: str,
+        interpolation: str = "bilinear"
+    ):
+        x_dim, y_dim, z_dim = self.target_image_shape
+        _memory_dict_xy = self._initialize_memory_dict(x_dim, y_dim, z_dim)
+
+        return self._apply_euler_parameters(
+                moving_image,
+                _memory_dict_xy,
+                interpolation,
+                problem_id
+        )
+
+    def _initialize_memory_dict(
+        self,
+        dim_1: int,
+        dim_2: int,
+        dim_3: int
+    ) -> Dict[str, torch.Tensor]:
+        _memory_dict = initialize(
+            np.zeros((dim_1, dim_2)).astype(np.float32),
+            np.zeros((dim_1, dim_2)).astype(np.float32),
+            np.zeros(dim_3),
+            np.zeros(dim_3),
+            np.zeros(dim_3),
+            dim_3,
+            self.device_name
+        )
+        return _memory_dict
+
+    def _apply_euler_parameters(
+        self,
+        moving_image_roi: np.ndarray,
+        memory_dict: Dict[str, torch.Tensor],
+        interpolation: str,
+        problem_id: str,
+        dimension: str = "xy",
+    ) -> np.ndarray:
+
+        best_transformation = torch.tensor(
+            self.euler_parameters_dict[problem_id][dimension]
+        ).to(self.device_name)
+
+        moving_image_roi = self._adjust_image_shape(
+                moving_image_roi,
+                dimension
+        )
+        if dimension == "xy":
+            axis = 2
+        elif dimension == "xz":
+            axis = 1
+        elif dimension == "yz":
+            axis = 0
+        transformed_moving_image_roi = transform_image_3d(
+                moving_image_roi,
+                memory_dict,
+                best_transformation,
+                self.device_name, # "cuda:2"
+                axis,
+                interpolation
+        )
+        translated_moving_image_roi = self._translate_image(
+                self._adjust_image_shape(
+                    transformed_moving_image_roi,
+                    "xy"
+                ),
+                problem_id
+        )
+
+        return translated_moving_image_roi
+
+    def _adjust_image_shape(
+        self,
+        image: np.ndarray,
+        dimension: str,
+    ) -> np.ndarray:
+
+        x_dim, y_dim, z_dim = self.target_image_shape
+        reshaping_dict = {
+            (y_dim, z_dim, x_dim): {
+                "xy": (2, 0, 1),
+                "xz": (2, 1, 0),
+                "yz": (0, 1, 2)
+            },
+            (x_dim, z_dim, y_dim): {
+                "xy": (0, 2, 1),
+                "yz": (2, 1, 0),
+                "xz": (0, 1, 2)
+            },
+            (x_dim, y_dim, z_dim): {
+                "yz": (1, 2, 0),
+                "xz": (0, 2, 1),
+                "xy": (0, 1, 2)
+            }
+        }
+        order = reshaping_dict[image.shape][dimension]
+
+        return np.transpose(image, order)
+
+    def _translate_image(
+        self,
+        image: np.ndarray,
+        problem_id: str
+    ) -> np.ndarray:
+
+        dz = self.euler_parameters_dict[problem_id]["dz"]
+        translated_image = np.full(self.target_image_shape, 0)
+
+        if dz < 0:
+            translated_image[:, :, :dz] = image[:, :, -dz:]
+        elif dz > 0:
+            translated_image[:, :, dz:] = image[:, :, :-dz]
+        elif dz == 0:
+            translated_image = image
+
+        return translated_image
+
---
+++ scripts/data_preparation/warp_roi.py
@@ -0,0 +1,178 @@
+from data_utils import get_cropped_image, write_to_json
+from tqdm import tqdm
+from typing import List, Dict, Tuple
+from warp import ImageWarper
+import glob
+import h5py
+import json
+import nibabel as nib
+import numpy as np
+import os
+
+
+class LabelWarper(ImageWarper):
+
+    def __init__(self, *args, **kwargs):
+
+        super().__init__(None, *args, **kwargs)
+        self.label_path = "/data1/prj_register/deepreg_labels"
+        self.bad_labels = []
+
+    def warp_label(
+        self,
+        simply_crop: bool,
+    ) -> Dict[str, np.ndarray]:
+        """ Warp the ROI images with the Euler parameters obtained from
+        preprocessing the registration problems for training and validation. """
+
+        if self._label_exists():
+            return self._preprocess_image_roi(simply_crop)
+        else:
+            print(f"{self.dataset_name} has no labels")
+            return {}
+
+    def _label_exists(self):
+
+        return self.registration_problem in \
+            os.listdir(f"{self.label_path}/{self.dataset_name}/register_labels/")
+
+    def _update_problem(self):
+        self.problem_id = f"{self.dataset_name}/{self._registration_problem}"
+
+    def _preprocess_image_roi(
+        self,
+        simply_crop: bool,
+    ) -> Dict[str, np.ndarray]:
+        """ Redefine this method for LabelWarper. """
+
+        roi_path = \
+            f"{self.label_path}/{self.dataset_name}/register_labels/{self.registration_problem}"
+        fixed_image_roi = nib.load(
+                f"{roi_path}/img_fixed.nii.gz").get_fdata().astype(np.float32)
+        moving_image_roi = nib.load(
+                f"{roi_path}/img_moving.nii.gz").get_fdata().astype(np.float32)
+
+        if self.nonzero_labels(fixed_image_roi) and self.nonzero_labels(moving_image_roi):
+
+            resized_fixed_image_roi = self._resize_image_roi(
+                    fixed_image_roi,
+                    self.CM_dict[self.problem_id]["fixed"]
+            )
+            resized_moving_image_roi = self._resize_image_roi(
+                    moving_image_roi,
+                    self.CM_dict[self.problem_id]["moving"]
+            )
+            if simply_crop:
+                return {
+                    "fixed_image_roi": resized_fixed_image_roi,
+                    "moving_image_roi": resized_moving_image_roi
+                }
+            else:
+                # pass interpolation method
+                euler_transformed_moving_image_roi = self._euler_transform_image_roi(
+                        resized_moving_image_roi
+                )
+                return {
+                    "fixed_image_roi": resized_fixed_image_roi,
+                    "moving_image_roi": resized_moving_image_roi,
+                    "euler_tfmed_moving_image_roi": euler_transformed_moving_image_roi
+                }
+        else:
+            return {}
+
+    def _resize_image_roi(
+        self,
+        image_roi: np.ndarray,
+        image_CM: List[int]
+    ) -> np.ndarray:
+
+        return get_cropped_image(
+                image_roi,
+                image_CM,
+                self.image_shape, -1).astype(np.float32)
+
+    def nonzero_labels(self, label_roi):
+
+        if len(np.unique(label_roi)) == 1:
+            self.bad_labels.append(self.registration_problem)
+            return False
+        else:
+            return True
+
+
+def generate_labels(
+        device_name: str,
+        target_image_shape: Tuple[int, int, int],
+        problem_file: str,
+        save_directory: str,
+        simply_crop: bool,
+    ):
+
+    warper = LabelWarper(
+        None,  # dataset_name and registration_problem are set later
+        None,
+        target_image_shape,
+        problem_file,
+        device_name,
+        simply_crop, # default set to False in warp.ImageWarper
+    )
+
+    with open(f"resources/{problem_file}.json", "r") as f:
+        problem_dict = json.load(f)
+    dataset_types = {
+        "train": list(problem_dict["train"].keys()),
+        "valid": list(problem_dict["valid"].keys()),
+        "test": list(problem_dict["test"].keys())
+    }
+    all_bad_problems = {"train": {}, "valid": {}, "test": {}}
+    for dataset_type, datasets in dataset_types.items():
+        all_bad_problems[dataset_type] = generate_label(
+                datasets,
+                dataset_type,
+                warper,
+                save_directory,
+                problem_dict,
+                simply_crop
+        )
+    tag = problem_file.split("_")[-1]
+    write_to_json(all_bad_problems, f"bad_registration_problems_{tag}")
+
+def generate_label(
+        datasets: List[str],
+        dataset_type: str,
+        warper: LabelWarper,
+        save_directory: str,
+        problem_dict: Dict[str, Dict[str, List[str]]],
+        simply_crop: bool,
+    ):
+    bad_problems = dict()
+
+    for dataset in datasets:
+        label_path = f"{save_directory}/{dataset_type}/nonaugmented/{dataset}"
+        problems = problem_dict[dataset_type][dataset]
+        warper.dataset_name = dataset
+
+        with h5py.File(f"{label_path}/moving_rois.h5", "w") as h5_m_file, \
+             h5py.File(f"{label_path}/fixed_rois.h5", "w") as h5_f_file:
+
+            for problem in tqdm(problems):
+                warper.registration_problem = problem
+                label_dict = warper.warp_label(simply_crop=simply_crop)
+                if len(label_dict) > 0:
+                    if simply_crop:
+                        moving_image_roi = label_dict["moving_image_roi"]
+                    else:
+                        moving_image_roi = label_dict["euler_tfmed_moving_image_roi"]
+                    h5_m_file.create_dataset(
+                            problem,
+                            data = moving_image_roi
+                    )
+                    fixed_image_roi = label_dict["fixed_image_roi"]
+                    h5_f_file.create_dataset(
+                            problem,
+                            data = fixed_image_roi
+                    )
+        print(f"{dataset} generated!")
+        bad_problems[dataset] = warper.bad_labels
+
+    return bad_problems
