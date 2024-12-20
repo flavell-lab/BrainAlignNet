b69db17c4bcd726b782588b51e92f6f322747e75
nalinir <naliniramanathan@gmail.com>
2024-12-20 12:22:16 -0500

    Removed all files for rebasing

--- scripts/data_preparation/test_demo_pipeline.py
+++
@@ -1,9 +0,0 @@
-
-from euler_register import EulerRegistrationProcessor
-
-target_image_shape = (284, 120, 64)
-save_directory = "/scratch/nar8991/computer_vision/output"
-problem_file = "/scratch/nar8991/computer_vision/BrainAlignNet/scripts/data_preparation/resources/registration_problems.json"
-
-
-
